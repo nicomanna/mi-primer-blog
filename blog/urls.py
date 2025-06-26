260d95f18e0cfa35039657d8ad189fecf6ab8003
nicomanna <nicomanna2017@gmail.com>
2025-06-25 22:12:23 -0300

    urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path('', views.lista_public, name=' lista_public'),
+    path('', views.lista_public, name='lista_public'),
+    path('evaluacion2/', views.evaluacion2, name='evaluacion2'),
 ]
