b3d393038c674b62bcafc58bd22b511f0e5666c7
nicomanna <nicomanna2017@gmail.com>
2025-06-25 23:54:54 -0300

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+
 class Publicacion(models.Model):
     autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     titulo = models.CharField(max_length=200)
@@ -13,4 +14,12 @@ class Publicacion(models.Model):
     def __str__(self):
         return self.titulo
 
+# Create your models here.
+class Marca(models.Model):
+    nombre = models.CharField(max_length=100)
+    fecha_fundacion = models.DateField()
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.nombre
 # Create your models here.
