f4554e25333dadd7ae1b27926c09d4c8a817fb76
nicomanna <nicomanna2017@gmail.com>
2025-06-25 23:48:34 -0300

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Publicacion
+from .models import Marca
+
 
 def lista_public(request):
     publicaciones = Publicacion.objects.filter(
         fecha_publicacion__lte=timezone.now()
     ).order_by('fecha_publicacion')
     return render(request, 'blog/lista_public.html', {'publicaciones': publicaciones})
+def lista_marcas(request):
+    marcas = Marca.objects.all()
+    return render(request, 'blog/lista_marcas.html', { 'marcas': marcas })
