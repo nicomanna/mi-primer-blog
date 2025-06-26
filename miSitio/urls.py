dcc0f58c948dcf97316b0dee16696c74cfa79aa9
nicomanna <nicomanna2017@gmail.com>
2025-06-25 22:10:35 -0300

    urls.py

--- miSitio/urls.py
+++ miSitio/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    path('', views.lista_public, name='lista_public'),  
+    path('evaluacion2/', views.evaluacion2, name='evaluacion2'),  
 ]
